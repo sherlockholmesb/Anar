9aabe34e4603bb5fa8c2fd96fab837c99db07e84
sherlockholmesb <127186299+sherlockholmesb@users.noreply.github.com>
2023-05-05 12:35:45 +0400

    Add files via upload

---
+++ NaNTech/main/js/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var c=document.getElementyByClassName("shifre");
+    if(c.type==="shifre"){
+         c.type="password";
+    } else {
+        c.type="password";
+    }
+   
+
+}
